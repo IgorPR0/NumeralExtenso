a12d707b310fadc34d018d6ba78bf91a668badaf
IgorPR0 <igortecnologia2020@gmail.com>
2020-10-08 18:48:36 -0300

    Resolvido NumeralExtenso

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Humanizer;
+
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Numero/Para/Letra//
+            int calculo;
+            string escrita;
+    
+            Console.Write("Conversor de Numeros para Letras: ");
+            calculo = int.Parse(Console.ReadLine());
+            escrita = calculo.ToWords();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(escrita);
+            Console.ResetColor();
+        }
+    }
+}
